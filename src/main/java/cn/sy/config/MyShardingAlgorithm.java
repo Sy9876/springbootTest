2f996bfe88916bc8276642389e6e1c2cb1bf7a24
shenyue <sheny@lzt.com.cn>
2018-05-18 19:34:17 +0800

    自定义分片策略，根据路由表分片（未完成）

---
+++ src/main/java/cn/sy/config/MyShardingAlgorithm.java
@@ -0,0 +1,64 @@
+package cn.sy.config;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import io.shardingjdbc.core.api.algorithm.sharding.PreciseShardingValue;
+import io.shardingjdbc.core.api.algorithm.sharding.standard.PreciseShardingAlgorithm;
+
+public class MyShardingAlgorithm implements PreciseShardingAlgorithm<Integer> {
+	private static Logger logger = Logger.getLogger(MyShardingAlgorithm.class);
+	
+	Map<Integer, String> routingMap = new HashMap<>();
+	
+	public MyShardingAlgorithm() {
+		routingMap.put(1, "t_order_1");
+		routingMap.put(2, "t_order_0");
+		routingMap.put(3, "t_order_1");
+		routingMap.put(4, "t_order_0");
+		routingMap.put(5, "t_order_1");
+		routingMap.put(6, "t_order_0");
+		routingMap.put(7, "t_order_1");
+		routingMap.put(8, "t_order_0");
+		
+	}
+	
+	@Override
+	public String doSharding(Collection<String> availableTargetNames, PreciseShardingValue<Integer> shardingValue) {
+		
+		logger.info("doSharding. ");
+		String atn = printAvailableTargetNames(availableTargetNames);
+		logger.info(" availableTargetNames: " + atn);
+		
+		String sv =  printShardingValue(shardingValue);
+		logger.info(" shardingValue: " + sv);
+		
+		Integer v = shardingValue.getValue();
+		String result = routingMap.get(v);
+		
+		logger.info(" routing result: " + result);
+		return result;
+	}
+
+	private String printAvailableTargetNames(Collection<String> availableTargetNames) {
+		StringBuffer sb = new StringBuffer();
+		for(String name : availableTargetNames) {
+			if(sb.length()>0) {
+				sb.append(", ");
+			}
+			sb.append(name);
+		}
+		return sb.toString();
+	}
+	private String printShardingValue(PreciseShardingValue<Integer> shardingValue) {
+		String s = new String();
+		s += "logicTableName=" + shardingValue.getLogicTableName();
+		s += ", columnName=" + shardingValue.getColumnName();
+		s += ", value=" + shardingValue.getValue();
+		
+		return s;
+	}
+}
